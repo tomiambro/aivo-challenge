5209f3c674fd42db11e0bba7e1f500121085fccb
Tomi <tomi.ambro94@gmail.com>
2021-01-14 20:42:40 -0300

    Added inequality filter and returns only countries

--- api/views.py
+++ api/views.py
@@ -8,23 +8,22 @@ import pandas as pd
 
 @api_view(['GET'])
 def endpoint(request):
-	df = pd.read_csv('aivo.csv')
 	if 'value' not in request.GET:
 		return Response(status=status.HTTP_400_BAD_REQUEST)
 
 	if 'indicator' not in request.GET:
-		indicator = 'Life satisfaction'
+		indicator = 'SW_LIFS'
 	else:
 		indicator = request.GET['indicator']
 
 	value = request.GET['value']
-
 	try:
 		value = float(value)
 	except Exception as e:
 		print(e)
 		return Response(status=status.HTTP_400_BAD_REQUEST)		
 
-	res = df[(df['Indicator'] == indicator) & (df['Value'] > float(value))]
-	# print(res)
-	return Response(res.to_json(), status=status.HTTP_200_OK)
+	df = pd.read_csv('aivo.csv')
+	res = df[(df['INDICATOR'] == indicator) & (df['Value'] > float(value)) & (df['INEQUALITY'] == 'TOT')]
+	print(res['Country'])
+	return Response(res['Country'].to_json(), status=status.HTTP_200_OK)
